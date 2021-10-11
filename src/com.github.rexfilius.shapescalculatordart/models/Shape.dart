bea3408a1872e49d01270a337f3c6a2384f31910
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2021-10-11 12:22:20 +0100

    First commit. Create model class Shape

---
+++ src/com.github.rexfilius.shapescalculatordart/models/Shape.dart
@@ -0,0 +1,11 @@
+class Shape {
+  var length = 0.0;
+  var breadth = 0.0;
+  var height = 0.0;
+  var sideLength = 0.0;
+  var sideLength1 = 0.0;
+  var sideLength2 = 0.0;
+  var topLength = 0.0;
+  var baseLength = 0.0;
+  var radius = 0.0;
+}
